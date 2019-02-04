842c830ff999ab33978dc85dc18600b49cb7a2a2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-02-05 00:30:55 +0200

    Display home page

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const http = require('http');
 const port = 3000;
+const handlers = require('./handlers');
 
 http.createServer((req, res) => {
-     res.writeHead(200, {
-          'Content-Type': 'text-plain'
-     });
-
-     res.write('Hello');
-     res.end();
-
+     for (let handler of handlers) {
+         if (!handler(req, res)) {
+              break;
+         } 
+     }
 }).listen(port);
