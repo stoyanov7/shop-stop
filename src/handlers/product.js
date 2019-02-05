62a5f5bc0ec76a7560f93f1c406928561b35d54b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-02-05 11:18:50 +0200

    Upload image for product

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -3,6 +3,8 @@ const database = require('../config/database');
 const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
+const multiparty = require('multiparty');
+const shortid = require('shortid');
 
 module.exports = (req, res) => {
    req.pathname = req.pathname || url.parse(req.url).pathname;
@@ -31,22 +33,58 @@ module.exports = (req, res) => {
          res.end();
       });
    } else if (req.pathname === '/products/add' && req.method === 'POST') {
-      let dataString = '';
+      let form = new multiparty.Form();
+      let products = {};
 
-      req.on('data', (data) => {
-         dataString += data;
+      form.on('part', p => {
+         if (p.filename) {
+            let dataString = '';
+
+            part.setEncoding('binary');
+
+            part.on('data', d => {
+               dataString += d;
+            });
+
+            part.on('end', () => {
+               let filename = shortid.generate();
+               let extension = part.filename.substring(part.filename.lastIndexOf('.'));
+               let filePath = `/content/images/${filename}${extension}`;
+
+               products.image = filePath;
+
+               fs.writeFile(`.${filePath}`, dataString, { encoding: 'ascii' }, (err) => {
+                  if (err) {
+                     console.log(err);
+                     return;
+                  }
+               });
+            });
+         } else {
+            part.setEncoding('utf-8');
+            let field = '';
+
+            part.on('data', d => {
+               field += d;
+            });
+
+            part.on('end', () => {
+               product[part.name] = field;
+            });
+         }
       });
 
-      req.on('end', () => {
-         let product = qs.parse(dataString);
-         database.products.add(product);
+      form.on('close', () => {
+         database.products.add(product); 
+         
+         res.writeHead(302, {
+            Location: '/'
+         });
+
+         res.end();
       });
 
-      res.writeHead(302, {
-         Location: '/'
-      });
-
-      res.end();
+      form.parse(req);
    } else {
       return true;
    }
