e5e6afdee10885a8de6916bca550a517e7b41b58
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-02-09 13:10:15 +0200

    Add new products form now is with select menu (with categories)

--- src/handlers/product.js
+++ src/handlers/product.js
@@ -4,6 +4,7 @@ const path = require('path');
 const multiparty = require('multiparty');
 const shortid = require('shortid');
 const Product = require('../models/Product');
+const Category = require('../models/Category');
 
 module.exports = (req, res) => {
    req.pathname = req.pathname || url.parse(req.url).pathname;
@@ -24,12 +25,26 @@ module.exports = (req, res) => {
             return;
          }
 
-         res.writeHead(200, {
-            'Content-Type': 'text/html'
-         });
+         Category.find().then((categories) => {
+            let replacement = '<select class="input-field" name="category">';
 
-         res.write(data);
-         res.end();
+            for (let category of categories) {
+               replacement += `$<option value="${category._id}">${category.name}</option>`;
+            }
+
+            replacement += '</select>';
+
+            let html = data
+               .toString()
+               .replace('{categories}', replacement);
+
+            res.writeHead(200, {
+               'Content-Type': 'text/html'
+            });
+
+            res.write(html);
+            res.end();
+         });
       });
    } else if (req.pathname === '/products/add' && req.method === 'POST') {
       let form = new multiparty.Form();
@@ -74,7 +89,12 @@ module.exports = (req, res) => {
       });
 
       form.on('close', () => {
-         Product.create(product).then(() => {
+         Product.create(product).then((insertedProduct) => {
+            Category.findById(product.category).then(category => {
+               category.products.push(insertedProduct._id);
+               category.save();
+            });
+            
             res.writeHead(302, {
                Location: '/'
             });
